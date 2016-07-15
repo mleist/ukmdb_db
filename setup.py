9a5993bd3ff2c1ccd32ce954f756eeccdb4bc912
Markus Leist <markus@lei.st>
2016-07-15 15:25:41 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='ukmdb_db',
-    version='0.0.1',
+    version='0.0.2',
     description="UKMDB Database Interface.",
     long_description=readme + '\n\n' + history,
     author="Markus Leist",
